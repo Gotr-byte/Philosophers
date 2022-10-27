e2652c8d7a86ed9ad98597b2467ed0f6eaa442cb
pbiederm <pbiederm@student.42wolfsburg.de>
2022-10-27 17:00:18 +0200

    added the mutex in the thread made a linked list of philosphers containing forks

--- Philosophers/philosophers.c
+++ Philosophers/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: pbiederm <pbiederm@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 10:30:01 by pbiederm          #+#    #+#             */
-/*   Updated: 2022/10/25 18:14:31 by pbiederm         ###   ########.fr       */
+/*   Updated: 2022/10/27 16:57:55 by pbiederm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,7 @@
 
 
 
-pthread_mutex_t eatex;
-// pthread_muted_t sleepex;
-
+// pthread_mutex_t eatex;
 
 void *eating(void *arg)
 {
@@ -31,39 +29,38 @@ void *eating(void *arg)
 	struct timeval 	start, end;
 	static int				i;
 
+	philosopher = (t_philosopher *)arg;
 	while (i < 3)
 	{
-		philosopher = (t_philosopher *)arg;
 		if (philosopher->sleep_time_curr > 0)
 		{
 			gettimeofday(&start, NULL);
 			usleep(philosopher->sleep_time_curr);
 			philosopher->sleep_time_curr = 0;
 			gettimeofday(&end, NULL);
-			printf("Current time is: %ld\nActual time taken to for %s to sleep is: %ld micro seconds\nSet time is: %d\n", \
-			(start.tv_sec * 1000000 + end.tv_usec),\
-			(char *)philosopher->name,\
-			(end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec),\
-			philosopher->sleep_time_set);
+			printf("%ld %s is sleeping\n", \
+			(start.tv_sec * 1000 + end.tv_usec / 1000),\
+			(char *)philosopher->name);
 		}
 		else
 		{
 			gettimeofday(&start, NULL);
-			printf("Current time is: %ld\n %s is taking his time thinking\n", \
-			(start.tv_sec * 1000000 + end.tv_usec),\
+			printf("%ld %s is taking his time thinking\n", \
+			(start.tv_sec * 1000 + end.tv_usec / 1000),\
 			(char *)philosopher->name);
 		}
-		pthread_mutex_lock(&eatex);
+		pthread_mutex_lock(&philosopher->next->fork);
+		pthread_mutex_lock(&philosopher->fork);
 		gettimeofday(&start, NULL);
 		usleep(philosopher->gorge_time);
 		philosopher->sleep_time_curr = philosopher->sleep_time_set;
 		gettimeofday(&end, NULL);
-		printf("Current time is: %ld\nActual time taken to for %s to eat is: %ld micro seconds\nSet time is: %d\n",\
-		(start.tv_sec * 1000000 + end.tv_usec),\
-		(char *)philosopher->name,\
-		(end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec),\
-		philosopher->gorge_time);
-		pthread_mutex_unlock(&eatex);
+		pthread_mutex_unlock(&philosopher->fork);
+		pthread_mutex_unlock(&philosopher->next->fork);
+		printf("%ld %s is eating\n",\
+		(start.tv_sec * 1000 + end.tv_usec / 1000),\
+		(char *)philosopher->name);
+	
 		i++;
 	}
 
@@ -72,15 +69,21 @@ void *eating(void *arg)
 
 int main (void)
 {
+
 	t_philosopher	*Konfucjusz = NULL;
 	t_philosopher	*Marek = NULL;
 	t_philosopher	*Platon = NULL;
-
-	pthread_mutex_init(&eatex, NULL);
-	// pthread_mutex_init(&sleepex, NULL);
+	
+	
+	Platon = (t_philosopher*)calloc(1, sizeof(t_philosopher));
 	Konfucjusz = (t_philosopher*)calloc(1, sizeof(t_philosopher));
 	Marek = (t_philosopher*)calloc(1, sizeof(t_philosopher));
-	Platon = (t_philosopher*)calloc(1, sizeof(t_philosopher));
+	Konfucjusz->next = Platon;
+	Platon->next = Marek;
+	Marek->next = Konfucjusz;
+	pthread_mutex_init(&(Konfucjusz->fork), NULL);
+	pthread_mutex_init(&(Marek->fork), NULL);
+	pthread_mutex_init(&(Platon->fork), NULL);
 	Konfucjusz->name = (char *)calloc(11, sizeof(char));
 	Marek->name = (char *)calloc(6, sizeof(char));
 	Platon->name = (char *)calloc(7, sizeof(char));
@@ -96,25 +99,15 @@ int main (void)
 	Marek->sleep_time_set = 2000000;
 	Platon->sleep_time_set = 2000000;
 
-	int result1 = pthread_create(&Konfucjusz->pt_id, NULL, eating, Konfucjusz);
-	int result2 = pthread_create(&Marek->pt_id, NULL, eating, Marek);
-	int result3 = pthread_create(&Platon->pt_id, NULL, eating, Platon);
-	
-	if (result1 || result2 || result3)
-	{
-		printf("The threads could not be created.\n");
-		exit(1);
-	}
+	pthread_create(&Konfucjusz->pt_id, NULL, eating, Konfucjusz);
+	pthread_create(&Marek->pt_id, NULL, eating, Marek);
+	pthread_create(&Platon->pt_id, NULL, eating, Platon);
 
-	result1 = pthread_join(Konfucjusz->pt_id, NULL);
-	result2 = pthread_join(Marek->pt_id, NULL);
-	result3 = pthread_join(Platon->pt_id, NULL);
-	pthread_mutex_destroy(&eatex);
-	// pthread_mutex_destroy(&sleepex);
-	if (result1 || result2 || result3)
-	{
-		printf("The threads could not be joined.\n");
-		exit (2);
-	}
+	pthread_join(Konfucjusz->pt_id, NULL);
+	pthread_join(Marek->pt_id, NULL);
+	pthread_join(Platon->pt_id, NULL);
+	pthread_mutex_destroy(&(Konfucjusz->fork));
+	pthread_mutex_destroy(&(Marek->fork));
+	pthread_mutex_destroy(&(Platon->fork));
 	return (0);
 }
--- Philosophers/philosophers.h
+++ Philosophers/philosophers.h
@@ -5,22 +5,16 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-
-typedef struct s_time
-{
-	long			curr_time;
-}t_time;
-//
 typedef struct	s_philo
 {
-	pthread_t	pt_id;
-	char		*name;
-	int			gorge_time;
-	int			sleep_time_set;
-	int			sleep_time_curr;
-	int			think_time;
-	int			time_to_die;
-	// t_time		*ptr;
-	//mutex to point to something
+	pthread_t		pt_id;
+	char			*name;
+	int				gorge_time;
+	int				sleep_time_set;
+	int				sleep_time_curr;
+	int				think_time;
+	int				time_to_die;
+	pthread_mutex_t	fork;
+	struct s_philo	*next;
 }t_philosopher;
 #endif
